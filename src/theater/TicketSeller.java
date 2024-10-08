c8ea3c55340d9bfa85b97cfee8275420aec52520
dbgys1127 <dbgys1127@gmail.com>
2024-10-08 22:38:22 +0900

    UML 추가

--- src/theater/TicketSeller.java
+++ src/theater/TicketSeller.java
@@ -10,7 +10,6 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        Ticket ticket = ticketOffice.getTicket();
-        ticketOffice.plusAmount(audience.buy(ticket));
+        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
     }
 }
